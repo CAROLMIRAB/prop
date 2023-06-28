a59c4b3c1ba40839c3670082291faa45836a1254
Carol Mirabal <carolmirabal27@gmail.com>
2023-06-27 20:42:23 -0400

    configuracion de respuestas estandar

---
+++ packages/api/app/Classes/ResponsesBody.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Classes;
+
+class ResponsesBody
+{
+
+    /**
+     * responseSuccess
+     *
+     * @param  mixed $message
+     * @param  mixed $code
+     * @param  mixed $result
+     * @return void
+     */
+    static public function responseSuccess($message, $code, $result)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+            'message' => $message,
+        ];
+        return response()->json($response, $code);
+    }
+
+    /**
+     * responseError
+     *
+     * @param  mixed $message
+     * @param  mixed $code
+     * @param  mixed $errorMessages
+     * @return void
+     */
+    static public function responseError($message, $code = 404, $errorMessages = [])
+    {
+        $response = [
+            'success' => false,
+            'message' => $message,
+            'code' => $code
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['data'] = $errorMessages;
+        }
+        return response()->json($response, $code);
+    }
+}
--- packages/api/app/Exceptions/Handler.php
+++ packages/api/app/Exceptions/Handler.php
@@ -2,7 +2,10 @@
 
 namespace App\Exceptions;
 
+use App\Classes\ResponsesBody;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -36,6 +39,15 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->renderable(function ($request) {
+            if ($request->wantsJson()) {
+                return ResponsesBody::responseError('No encontrado', 404, []);
+            }
+            return ResponsesBody::responseError('No encontrado', 404, []);
+        });
+        $this->renderable(function ($request) {
+            return ResponsesBody::responseError('No encontrado', 404, []);
         });
     }
 }
